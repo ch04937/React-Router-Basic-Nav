f132089b1ac7bfcb53347a3c572d07daf016d8cf
ch04937 <97hernandez.c@gmail.com>
2019-07-22 13:00:36 -0700

    'Declaring routes'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
-  <div>
+  <div className = 'App'>
     <Navigation />
+    <Route path='/' exact component={} />
+    <Route path='/about' component={} />
+    <Route path='/contact' component={} />
+
   </div>
 );
 
